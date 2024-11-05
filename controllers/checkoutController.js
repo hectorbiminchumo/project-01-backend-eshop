697a56f321b5c2c0a3e3ca3dcbbab05ee813be7b
Hector <hectorbiminchumo@gmail.com>
2024-11-05 00:12:01 -0500

    changing checkouController

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -2,16 +2,20 @@
 const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 exports.createCheckoutSession = async (req, res) => {
+
+    const { cart } = req.body
     
     const session = await stripe.checkout.sessions.create({
-        line_items: [],
+        line_items: cart,
         mode: 'payment',
         success_url: "http://localhost:3000/?success=true",
         cancel_url: "http://localhost:3000/?canceled=true"
 
     })
 
-    res.send("hola")
+    res.json({
+        session: session
+    })
 
 
 
